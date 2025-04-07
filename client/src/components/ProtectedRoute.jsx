df6a7583b1f44886e91ca979ae0fae6f43e45464
jordankageorgieva <jo_georgieva@abv.bg>
2025-04-07 12:08:18 +0300

    routing error fix

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,14 +1,25 @@
-import { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
+import { useContext, useEffect } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
 import { UserContext } from '../hookContext/userContext';
 
 export default function ProtectedRoute({ children }) {
     const { accessToken, putLoginActionData } = useContext(UserContext);
 
     console.log("ProtectedRoute isAuthenticated: " + accessToken);
-   
+    const navigate = useNavigate();
+    if (accessToken === undefined) {
+        console.log("ProtectedRoute isAuthenticated is undefined: " + accessToken);
+    }
     // Check if the user is authenticated, if not redirect to login page
     // If the user is authenticated, render the children components
-    // and pass the authentication data to the children components
-    return accessToken ? children :  <Navigate to="/login" replace />;
+    // If the user is not authenticated, redirect to the login page
+    
+    useEffect(() => {
+        if (!accessToken) {
+            console.log("ProtectedRoute isAuthenticated is undefined: " + accessToken);
+            navigate('/login', { replace: true });
+        } 
+    }, [accessToken, navigate]);
+
+    return accessToken ? children : null;
 };
