f18e57a5f0093d6085e483ef994063d6954b7555
jordankageorgieva <jo_georgieva@abv.bg>
2025-12-16 15:19:39 +0200

    fast api add

---
+++ backend/api/index.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://softuni-react-exam-2025-test.vercel.app/"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/health")
+def health():
+    return {"status": "ok"}
+
+@app.get("/api/hello")
+def hello():
+    return {"message": "Hello from FastAPI on Vercel"}
