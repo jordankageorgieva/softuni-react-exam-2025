42f52b5541cf070003cb19a5a483a2d68de3289d
jordankageorgieva <jo_georgieva@abv.bg>
2025-12-17 12:06:57 +0200

    remove allow origins

--- backend/api/index.py
+++ backend/api/index.py
@@ -6,10 +6,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://softuni-react-exam-2025-test.vercel.app"
-    ],
+    # allow_origins=[
+    #     "http://localhost:5173",
+    #     "https://softuni-react-exam-2025-test.vercel.app"
+    # ],
+    allow_origins=["*"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
